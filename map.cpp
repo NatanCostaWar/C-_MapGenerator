2fcd25d4f2763c3645b5b6fc8cb9de0075069e39
NatanCosta <natancosta10-2011@hotmail.com>
2019-09-20 21:06:49 -0300

    translating code to english

--- map.cpp
+++ map.cpp
@@ -14,37 +14,37 @@ int main(){
 	map.open ("map.txt", std::fstream::in | std::fstream::out | std::fstream::app);
 	
 	//Creating the Map
-	int LARGURA= 100;
-	int ALTURA = 60;
+	int width= 100;
+	int height = 60;
 	
-	char AGUA  = '.';
-	char TERRA = 'i';
-	char AREIA = 'T';
-	char MONTE = 'M';
-	char NEVE  = 'N';
-	char RAZO  = 'R';
+	char WATER  = '.';
+	char GRASS = 'i';
+	char SAND = 'T';
+	char MOUNTAIN = 'M';
+	char SNOW  = 'N';
+	char SHALLOW  = 'R';
 	
 	
 	
-	cout<<"LARGURA DO MAPA (PADRAO 100) (VALOR MAX 1000):";
-	cin>>LARGURA;
-	cout<<"ALTURA  DO MAPA (PADRAO 60)  (VALOR MAX 1000):";
-	cin>>ALTURA;
-	char mapa [LARGURA][ALTURA];
+	cout<<"map width (standard: 100) (max: 1000):";
+	cin>>width;
+	cout<<"map height (standard: 60)  (max: 1000):";
+	cin>>height;
+	char mapa [width][height];
 	
 	//Filling the map with water
-	for(int j=0;j<ALTURA;j++){
-		for(int i=0;i<LARGURA;i++){
-			mapa[i][j] = AGUA;
+	for(int j=0;j<height;j++){
+		for(int i=0;i<width;i++){
+			mapa[i][j] = WATER;
 		}
 	}
 	
 	
 	//Generating Initial Land Points
-	for(int j=0;j<ALTURA;j++){
-		for(int i=0;i<LARGURA;i++){
+	for(int j=0;j<height;j++){
+		for(int i=0;i<width;i++){
 			int x = rand() % 300;
-			if(x < 4 && i>5 && i<LARGURA-5 && j>5 && j<ALTURA-5){
+			if(x < 4 && i>5 && i<width-5 && j>5 && j<height-5){
 				mapa[i][j] = '1';
 			}
 		}
@@ -52,8 +52,8 @@ int main(){
 		
 	
 	//Generating Lands From Initial Points
-	for(int j=0;j<ALTURA;j++){
-		for(int i=0;i<LARGURA;i++){
+	for(int j=0;j<height;j++){
+		for(int i=0;i<width;i++){
 			if(mapa[i][j] == '1'){
 				for(int a=i-5;a<=i+5;a++){
 					for(int b=j-5;b<=j+5;b++){
@@ -61,7 +61,7 @@ int main(){
 						}else{
 							int x = rand() % 100;
 							if(x > 50){
-								mapa[a][b]= TERRA;
+								mapa[a][b]= GRASS;
 							}	
 						}	
 					}
@@ -71,53 +71,53 @@ int main(){
 	}
 	
 	//Removing Initial Land Points
-	for(int j=0;j<ALTURA;j++){
-		for(int i=0;i<LARGURA;i++){
+	for(int j=0;j<height;j++){
+		for(int i=0;i<width;i++){
 			if(mapa[i][j] == '1'){
-				mapa[i][j]=TERRA;
+				mapa[i][j]=GRASS;
 			}		
 		}
 	}
 	
 	
 	//Filling Map Edges With Water
-	for(int j=0;j<ALTURA;j++){
-		for(int i=0;i<LARGURA;i++){
-			if(i < 3 or i>LARGURA-3 or j<3 or j>ALTURA-3){
-				mapa[i][j]=AGUA;
+	for(int j=0;j<height;j++){
+		for(int i=0;i<width;i++){
+			if(i < 3 or i>width-3 or j<3 or j>height-3){
+				mapa[i][j]=WATER;
 			}		
 		}
 	}
 	
 	//Filling with Land Water points Btween Land Points
-	for(int j=0;j<ALTURA;j++){
-		for(int i=0;i<LARGURA;i++){
-			//AGUA ENTRE DUAS TERRAS
-			if(mapa[i][j] == TERRA && mapa[i+1][j] == AGUA && mapa[i+2][j] == TERRA){
-				mapa[i+1][j] = TERRA;
+	for(int j=0;j<height;j++){
+		for(int i=0;i<width;i++){
+			//WATER ENTRE DUAS GRASSS
+			if(mapa[i][j] == GRASS && mapa[i+1][j] == WATER && mapa[i+2][j] == GRASS){
+				mapa[i+1][j] = GRASS;
 			}
-			if(mapa[i][j] == TERRA && mapa[i-1][j] == AGUA && mapa[i-2][j] == TERRA){
-				mapa[i-1][j] = TERRA;
+			if(mapa[i][j] == GRASS && mapa[i-1][j] == WATER && mapa[i-2][j] == GRASS){
+				mapa[i-1][j] = GRASS;
 			}
-			if(mapa[i][j] == TERRA && mapa[i][j+1] == AGUA && mapa[i][j+2] == TERRA){
-				mapa[i][j+1] = TERRA;
+			if(mapa[i][j] == GRASS && mapa[i][j+1] == WATER && mapa[i][j+2] == GRASS){
+				mapa[i][j+1] = GRASS;
 			}
-			if(mapa[i][j] == TERRA && mapa[i][j-1] == AGUA && mapa[i][j-2] == TERRA){
-				mapa[i][j-1] = TERRA;
+			if(mapa[i][j] == GRASS && mapa[i][j-1] == WATER && mapa[i][j-2] == GRASS){
+				mapa[i][j-1] = GRASS;
 			}
-			//DUAS AGUAS ENTRE DUAS TERRAS
-			if(mapa[i][j] == TERRA && mapa[i+1][j] == AGUA && mapa[i+2][j] == AGUA && mapa[i+3][j] == TERRA){
-				mapa[i+1][j] = TERRA;
-				mapa[i+2][j] = TERRA;
-			}if(mapa[i][j] == TERRA && mapa[i-1][j] == AGUA && mapa[i-2][j] == AGUA && mapa[i-3][j] == TERRA){
-				mapa[i-1][j] = TERRA;
-				mapa[i-2][j] = TERRA;
-			}if(mapa[i][j] == TERRA && mapa[i][j+1] == AGUA && mapa[i][j+2] == AGUA && mapa[i][j+3] == TERRA){
-				mapa[i][j+1] = TERRA;
-				mapa[i][j+2] = TERRA;
-			}if(mapa[i][j] == TERRA && mapa[i][j-1] == AGUA && mapa[i][j-2] == AGUA && mapa[i][j-3] == TERRA){
-				mapa[i][j+1] = TERRA;
-				mapa[i][j+2] = TERRA;
+			//DUAS WATERS ENTRE DUAS GRASSS
+			if(mapa[i][j] == GRASS && mapa[i+1][j] == WATER && mapa[i+2][j] == WATER && mapa[i+3][j] == GRASS){
+				mapa[i+1][j] = GRASS;
+				mapa[i+2][j] = GRASS;
+			}if(mapa[i][j] == GRASS && mapa[i-1][j] == WATER && mapa[i-2][j] == WATER && mapa[i-3][j] == GRASS){
+				mapa[i-1][j] = GRASS;
+				mapa[i-2][j] = GRASS;
+			}if(mapa[i][j] == GRASS && mapa[i][j+1] == WATER && mapa[i][j+2] == WATER && mapa[i][j+3] == GRASS){
+				mapa[i][j+1] = GRASS;
+				mapa[i][j+2] = GRASS;
+			}if(mapa[i][j] == GRASS && mapa[i][j-1] == WATER && mapa[i][j-2] == WATER && mapa[i][j-3] == GRASS){
+				mapa[i][j+1] = GRASS;
+				mapa[i][j+2] = GRASS;
 			}
 			
 			
@@ -125,19 +125,19 @@ int main(){
 	}
 	
 	//Generating beaches
-	for(int j=0;j<ALTURA;j++){
-		for(int i=0;i<LARGURA;i++){
-			if(mapa[i][j] == AGUA && mapa[i+1][j] == TERRA && mapa[i+2][j] == TERRA && mapa[i+3][j]==TERRA){
-				mapa[i+1][j] = AREIA;
+	for(int j=0;j<height;j++){
+		for(int i=0;i<width;i++){
+			if(mapa[i][j] == WATER && mapa[i+1][j] == GRASS && mapa[i+2][j] == GRASS && mapa[i+3][j]==GRASS){
+				mapa[i+1][j] = SAND;
 				
-			}if(mapa[i][j] == AGUA && mapa[i-1][j] == TERRA && mapa[i-2][j] == TERRA && mapa[i-3][j]==TERRA){
-				mapa[i-1][j] = AREIA;
+			}if(mapa[i][j] == WATER && mapa[i-1][j] == GRASS && mapa[i-2][j] == GRASS && mapa[i-3][j]==GRASS){
+				mapa[i-1][j] = SAND;
 				
-			}if(mapa[i][j] == AGUA && mapa[i][j+1] == TERRA && mapa[i][j+2] == TERRA && mapa[i][j+3]==TERRA){
-				mapa[i][j+1] = AREIA;
+			}if(mapa[i][j] == WATER && mapa[i][j+1] == GRASS && mapa[i][j+2] == GRASS && mapa[i][j+3]==GRASS){
+				mapa[i][j+1] = SAND;
 				
-			}if(mapa[i][j] == AGUA && mapa[i][j-1] == TERRA && mapa[i][j-2] == TERRA && mapa[i][j-3]==TERRA){
-				mapa[i][j-1] = AREIA;
+			}if(mapa[i][j] == WATER && mapa[i][j-1] == GRASS && mapa[i][j-2] == GRASS && mapa[i][j-3]==GRASS){
+				mapa[i][j-1] = SAND;
 				
 			}
 				
@@ -147,23 +147,23 @@ int main(){
 	
 	
 	//Generating shallow
-	for(int j=0;j<ALTURA;j++){
-		for(int i=0;i<LARGURA;i++){
-			if(mapa[i][j] == AREIA && mapa[i+1][j] == AGUA && mapa[i+2][j] == AGUA){
-				mapa[i+1][j] = RAZO;
-				mapa[i+2][j] = RAZO;
+	for(int j=0;j<height;j++){
+		for(int i=0;i<width;i++){
+			if(mapa[i][j] == SAND && mapa[i+1][j] == WATER && mapa[i+2][j] == WATER){
+				mapa[i+1][j] = SHALLOW;
+				mapa[i+2][j] = SHALLOW;
 			}
-			if(mapa[i][j] == AREIA && mapa[i-1][j] == AGUA && mapa[i-2][j] == AGUA){
-				mapa[i-1][j] = RAZO;
-				mapa[i-2][j] = RAZO;
+			if(mapa[i][j] == SAND && mapa[i-1][j] == WATER && mapa[i-2][j] == WATER){
+				mapa[i-1][j] = SHALLOW;
+				mapa[i-2][j] = SHALLOW;
 			}
-			if(mapa[i][j] == AREIA && mapa[i][j+1] == AGUA && mapa[i][j+2] == AGUA){
-				mapa[i][j+1] = RAZO;
-				mapa[i][j+2] = RAZO;
+			if(mapa[i][j] == SAND && mapa[i][j+1] == WATER && mapa[i][j+2] == WATER){
+				mapa[i][j+1] = SHALLOW;
+				mapa[i][j+2] = SHALLOW;
 			}
-			if(mapa[i][j] == AREIA && mapa[i][j-1] == AGUA && mapa[i][j-2] == AGUA){
-				mapa[i][j-1] = RAZO;
-				mapa[i][j-2] = RAZO;
+			if(mapa[i][j] == SAND && mapa[i][j-1] == WATER && mapa[i][j-2] == WATER){
+				mapa[i][j-1] = SHALLOW;
+				mapa[i][j-2] = SHALLOW;
 			}
 			
 		}
@@ -172,21 +172,21 @@ int main(){
 	
 	
 	//Generating Mountains
-	for(int j=0;j<ALTURA;j++){
-		for(int i=0;i<LARGURA;i++){
-			if(mapa[i][j] == TERRA){
+	for(int j=0;j<height;j++){
+		for(int i=0;i<width;i++){
+			if(mapa[i][j] == GRASS){
 				int quant=0;
 				int inicio=0;
-				for(int y=i;y<LARGURA;y++){
-					if(mapa[y][j]==TERRA){
+				for(int y=i;y<width;y++){
+					if(mapa[y][j]==GRASS){
 						quant++;
-						if(mapa[y-1][j]!=TERRA){
+						if(mapa[y-1][j]!=GRASS){
 							inicio=y;
 						}
 					}else{
 						if(quant>8){
 							for(int u =4;u<quant-4;u++){
-								mapa[inicio+u][j] = MONTE;
+								mapa[inicio+u][j] = MOUNTAIN;
 							}
 						}
 						quant=0;
@@ -197,40 +197,40 @@ int main(){
 		}
 	}
 	
-	for(int j=0;j<ALTURA;j++){
-		for(int i=0;i<LARGURA;i++){
-			if(mapa[i][j]==AREIA && mapa[i][j+1]==MONTE or mapa[i][j]==AREIA && mapa[i][j+2]==MONTE or mapa[i][j]==AREIA && mapa[i][j+3]==MONTE or mapa[i][j]==AREIA && mapa[i][j+4]==MONTE){
-				mapa[i][j+1]=TERRA;
-				mapa[i][j+2]=TERRA;
-				mapa[i][j+3]=TERRA;
-				mapa[i][j+4]=TERRA;
+	for(int j=0;j<height;j++){
+		for(int i=0;i<width;i++){
+			if(mapa[i][j]==SAND && mapa[i][j+1]==MOUNTAIN or mapa[i][j]==SAND && mapa[i][j+2]==MOUNTAIN or mapa[i][j]==SAND && mapa[i][j+3]==MOUNTAIN or mapa[i][j]==SAND && mapa[i][j+4]==MOUNTAIN){
+				mapa[i][j+1]=GRASS;
+				mapa[i][j+2]=GRASS;
+				mapa[i][j+3]=GRASS;
+				mapa[i][j+4]=GRASS;
 			}
-			if(mapa[i][j]==AREIA && mapa[i][j-1]==MONTE or mapa[i][j]==AREIA && mapa[i][j-2]==MONTE or mapa[i][j]==AREIA && mapa[i][j-3]==MONTE or mapa[i][j]==AREIA && mapa[i][j-4]==MONTE){
-				mapa[i][j-1]=TERRA;
-				mapa[i][j-2]=TERRA;
-				mapa[i][j-3]=TERRA;
-				mapa[i][j-4]=TERRA;
+			if(mapa[i][j]==SAND && mapa[i][j-1]==MOUNTAIN or mapa[i][j]==SAND && mapa[i][j-2]==MOUNTAIN or mapa[i][j]==SAND && mapa[i][j-3]==MOUNTAIN or mapa[i][j]==SAND && mapa[i][j-4]==MOUNTAIN){
+				mapa[i][j-1]=GRASS;
+				mapa[i][j-2]=GRASS;
+				mapa[i][j-3]=GRASS;
+				mapa[i][j-4]=GRASS;
 			}
 		}
 	}
 	
 	
 	//Generating Snow
-	for(int j=0;j<ALTURA;j++){
-		for(int i=0;i<LARGURA;i++){
-			if(mapa[i][j] == MONTE){
+	for(int j=0;j<height;j++){
+		for(int i=0;i<width;i++){
+			if(mapa[i][j] == MOUNTAIN){
 				int quant=0;
 				int inicio=0;
-				for(int y=i;y<LARGURA;y++){
-					if(mapa[y][j]==MONTE){
+				for(int y=i;y<width;y++){
+					if(mapa[y][j]==MOUNTAIN){
 						quant++;
-						if(mapa[y-1][j]!=MONTE){
+						if(mapa[y-1][j]!=MOUNTAIN){
 							inicio=y;
 						}
 					}else{
 						if(quant>10){
 							for(int u =5;u<quant-5;u++){
-								mapa[inicio+u][j] = NEVE;
+								mapa[inicio+u][j] = SNOW;
 							}
 						}
 						quant=0;
@@ -241,29 +241,29 @@ int main(){
 		}
 	}
 	
-	for(int j=0;j<ALTURA;j++){
-		for(int i=0;i<LARGURA;i++){
-			if(mapa[i][j]==TERRA && mapa[i][j+1]==NEVE or mapa[i][j]==TERRA && mapa[i][j+2]==NEVE or mapa[i][j]==TERRA && mapa[i][j+3]==NEVE or mapa[i][j]==TERRA && mapa[i][j+4]==NEVE or mapa[i][j]==TERRA && mapa[i][j+5]==NEVE){
-				mapa[i][j+1]=MONTE;
-				mapa[i][j+2]=MONTE;
-				mapa[i][j+3]=MONTE;
-				mapa[i][j+4]=MONTE;
-				mapa[i][j+5]=MONTE;
+	for(int j=0;j<height;j++){
+		for(int i=0;i<width;i++){
+			if(mapa[i][j]==GRASS && mapa[i][j+1]==SNOW or mapa[i][j]==GRASS && mapa[i][j+2]==SNOW or mapa[i][j]==GRASS && mapa[i][j+3]==SNOW or mapa[i][j]==GRASS && mapa[i][j+4]==SNOW or mapa[i][j]==GRASS && mapa[i][j+5]==SNOW){
+				mapa[i][j+1]=MOUNTAIN;
+				mapa[i][j+2]=MOUNTAIN;
+				mapa[i][j+3]=MOUNTAIN;
+				mapa[i][j+4]=MOUNTAIN;
+				mapa[i][j+5]=MOUNTAIN;
 			}
-			if(mapa[i][j]==TERRA && mapa[i][j-1]==NEVE or mapa[i][j]==TERRA && mapa[i][j-2]==NEVE or mapa[i][j]==TERRA && mapa[i][j-3]==NEVE or mapa[i][j]==TERRA && mapa[i][j-4]==NEVE or mapa[i][j]==TERRA && mapa[i][j-5]==NEVE){
-				mapa[i][j-1]=MONTE;
-				mapa[i][j-2]=MONTE;
-				mapa[i][j-3]=MONTE;
-				mapa[i][j-4]=MONTE;
-				mapa[i][j-5]=MONTE;
+			if(mapa[i][j]==GRASS && mapa[i][j-1]==SNOW or mapa[i][j]==GRASS && mapa[i][j-2]==SNOW or mapa[i][j]==GRASS && mapa[i][j-3]==SNOW or mapa[i][j]==GRASS && mapa[i][j-4]==SNOW or mapa[i][j]==GRASS && mapa[i][j-5]==SNOW){
+				mapa[i][j-1]=MOUNTAIN;
+				mapa[i][j-2]=MOUNTAIN;
+				mapa[i][j-3]=MOUNTAIN;
+				mapa[i][j-4]=MOUNTAIN;
+				mapa[i][j-5]=MOUNTAIN;
 			}
 		}
 	}
 	
 	
 	//Putting The Map In Archive
-	for(int j=0;j<ALTURA;j++){
-		for(int i=0;i<LARGURA;i++){
+	for(int j=0;j<height;j++){
+		for(int i=0;i<width;i++){
 			map << mapa[i][j];
 		}
 		map << "\n";
@@ -276,22 +276,22 @@ int main(){
 	map_img.write("P3",2);
 	map_img.write(" ",1);
 	std::stringstream v1;
-  	v1 << LARGURA;
+  	v1 << width;
   	cout << sizeof(v1.str().c_str());
 	map_img.write(v1.str().c_str(),v1.str().length());
 	map_img.write(" ",1);
 	std::stringstream v2;
-  	v2 << ALTURA;
+  	v2 << height;
 	map_img.write(v2.str().c_str(),v1.str().length());
 	map_img.write(" ",1);
 	map_img.write("255",3);
 	map_img.write("\n",1);
-	
+	 
 	//Generating PPM Image
-	for(int j=0;j<ALTURA;j++){
-		for(int i=0;i<LARGURA;i++){
+	for(int j=0;j<height;j++){
+		for(int i=0;i<width;i++){
 			
-			if(mapa[i][j] == TERRA){
+			if(mapa[i][j] == GRASS){
 				int x = rand() % 100;
 				if(x>50){
 					map_img.write("86",3);
@@ -308,35 +308,35 @@ int main(){
 					map_img.write("176",3);
 					map_img.write(" ",1);
 				}
-			}else if(mapa[i][j] == AGUA){
+			}else if(mapa[i][j] == WATER){
 				map_img.write("14",3);
 				map_img.write(" ",1);
 				map_img.write("106",3);
 				map_img.write(" ",1);
 				map_img.write("131",3);
 				map_img.write(" ",1);
-			}else if(mapa[i][j] == RAZO){
+			}else if(mapa[i][j] == SHALLOW){
 				map_img.write("179",3);
 				map_img.write(" ",1);
 				map_img.write("228",3);
 				map_img.write(" ",1);
 				map_img.write("207",3);
 				map_img.write(" ",1);
-			}else if(mapa[i][j] == AREIA){
+			}else if(mapa[i][j] == SAND){
 				map_img.write("229",3);
 				map_img.write(" ",1);
 				map_img.write("229",3);
 				map_img.write(" ",1);
 				map_img.write("179",3);
 				map_img.write(" ",1);
-			}else if(mapa[i][j] == MONTE){
+			}else if(mapa[i][j] == MOUNTAIN){
 				map_img.write("61",3);
 				map_img.write(" ",1);
 				map_img.write("58",3);
 				map_img.write(" ",1);
 				map_img.write("51",3);
 				map_img.write(" ",1);
-			}else if(mapa[i][j] == NEVE){
+			}else if(mapa[i][j] == SNOW){
 				map_img.write("207",3);
 				map_img.write(" ",1);
 				map_img.write("207",3);
